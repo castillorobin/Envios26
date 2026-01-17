3156226786836c109b56f81561b22c74d988f953
Robin Castillo <robin.castillo@catolica.edu.sv>
2026-01-16 23:05:40 -0600

    cambios en menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,27 +1,4 @@
 
-                        <li class="menu-title">General</li>
-
-                        <li class="nav-item">
-                            <a class="nav-link menu-arrow" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                                <span class="nav-icon">
-                                    <iconify-icon icon="iconamoon:home-duotone"><template shadowrootmode="open"><style data-style="data-style">:host{display:inline-block;vertical-align:0}span,svg{display:block}</style><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="currentColor"><path d="M17 21H7a2 2 0 0 1-2-2v-9l7-7l7 7v9a2 2 0 0 1-2 2" opacity=".16"></path><path d="M20 10a1 1 0 1 0-2 0zM6 10a1 1 0 0 0-2 0zm14.293 2.707a1 1 0 0 0 1.414-1.414zM12 3l.707-.707a1 1 0 0 0-1.414 0zm-9.707 8.293a1 1 0 1 0 1.414 1.414zM7 22h10v-2H7zm13-3v-9h-2v9zM6 19v-9H4v9zm15.707-7.707l-9-9l-1.414 1.414l9 9zm-10.414-9l-9 9l1.414 1.414l9-9zM17 22a3 3 0 0 0 3-3h-2a1 1 0 0 1-1 1zM7 20a1 1 0 0 1-1-1H4a3 3 0 0 0 3 3z"></path></g></svg></template></iconify-icon>
-                                </span>
-                                <span class="nav-text"> Dashboards </span>
-                            </a>
-                            <div class="collapse" id="sidebarDashboards">
-                                <ul class="nav sub-navbar-nav">
-                                    <li class="sub-nav-item">
-                                        <a class="sub-nav-link" href="index.html">Analytics</a>
-                                    </li>
-                                    <li class="sub-nav-item">
-                                        <a class="sub-nav-link" href="dashboard-finance.html">Finance</a>
-                                    </li>
-                                    <li class="sub-nav-item">
-                                        <a class="sub-nav-link" href="dashboard-sales.html">Sales</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
 
                         <li class="menu-title">Apps</li>
 
